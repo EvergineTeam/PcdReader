747fc2525158e8f4c7e2e2509ce88b4086f2fce1
Marcos Cobeña Morián <mcobena@plainconcepts.com>
2023-03-24 13:59:41 +0100

    improve performance
    minor refactors

--- PcdReader.cs
+++ PcdReader.cs
@@ -1,125 +1,93 @@
-﻿using System.Text;
-using System.Text.RegularExpressions;
-
-// PCD file format: https://pcl.readthedocs.io/projects/tutorials/en/master/pcd_file_format.html#pcd-file-format
+﻿// PCD file format: https://pcl.readthedocs.io/projects/tutorials/en/master/pcd_file_format.html#pcd-file-format
 class PcdReader
 {
-    Header header;
-    List<Point3D> point3Ds;
-    int pointCount = 0;
+    private Header header;
+
+    private Point3D[] points;
+
+    private int pointCount = 0;
 
     public int FileSizeBytes { get; private set; }
 
     public int PointCount => pointCount;
 
-    public IEnumerable<Point3D> Points => point3Ds;
+    public IEnumerable<Point3D> Points => points;
 
-    public (string x, string y, string z) ViewpointTranslation { get; private set; }
+    public Header Header => header;
 
-    public (string w, string x, string y, string z) ViewpointQuaternion { get; private set; }
-
-    /// <summary>
-    /// 获取头部信息
-    /// </summary>
-    /// <param name="s"></param>
-    private void GetHeader(string s)
+    public void ReadPcdFile(string fileName)
     {
-        header = new Header();
-        Regex reg_VERSION = new Regex("VERSION .*");  //版本
-        Regex reg_FIELDS = new Regex("FIELDS .*");  //字段
-        Regex reg_SIZE = new Regex("SIZE .*");  //数据大小
-        Regex reg_TYPE = new Regex("TYPE .*");  //存储数据的格式 F-Float U-uint
-        Regex reg_COUNT = new Regex("COUNT .*");
-        Regex reg_WIDTH = new Regex("WIDTH .*");
-        Regex reg_HEIGHT = new Regex("HEIGHT .*");
-        Regex reg_VIEWPOINT = new Regex("VIEWPOINT .*");
-        Regex reg_POINTS = new Regex("POINTS .*");  // Number of points
-        Regex reg_DATA = new Regex("DATA .*");  //数据类型
-
-        Match m_VERSION = reg_VERSION.Match(s);
-        header.VERSION = m_VERSION.Value;
-
-        header.FistLine = "# .PCD v" + header.VERSION.Split(' ')[1] + " - Point Cloud Data file format";
-
-        Match m_FIELDS = reg_FIELDS.Match(s);
-        header.FIELDS = m_FIELDS.Value;
-
-        Match m_SIZE = reg_SIZE.Match(s);
-        header.SIZE = m_SIZE.Value;
-
-        Match m_TYPE = reg_TYPE.Match(s);
-        header.TYPE = m_TYPE.Value;
-
-        Match m_COUNT = reg_COUNT.Match(s);
-        header.COUNT = m_COUNT.Value;
-
-        Match m_WIDTH = reg_WIDTH.Match(s);
-        header.WIDTH = m_WIDTH.Value;
-
-        Match m_HEIGHT = reg_HEIGHT.Match(s);
-        header.HEIGHT = m_HEIGHT.Value;
-
-        Match m_VIEWPOINT = reg_VIEWPOINT.Match(s);
-        header.VIEWPOINT = m_VIEWPOINT.Value;
-        var viewpointParts = header.VIEWPOINT.Split(' ').Skip(1);
-        ViewpointTranslation = (
-            viewpointParts.ElementAt(0),
-            viewpointParts.ElementAt(1),
-            viewpointParts.ElementAt(2));
-        ViewpointQuaternion = (
-            viewpointParts.ElementAt(3),
-            viewpointParts.ElementAt(4),
-            viewpointParts.ElementAt(5),
-            viewpointParts.ElementAt(6));
-
-        Match m_POINTS = reg_POINTS.Match(s);
-        header.POINTS = m_POINTS.Value;
-
-        Match m_DATA = reg_DATA.Match(s);
-        header.DATA = m_DATA.Value;
-    }
-
-    /// <summary>
-    /// 读取 binary 和 binary_compressed 格式的 pcd 文件
-    /// </summary>
-    public void ReadPcdFile(string path)
-    {
-        byte[] bytes = File.ReadAllBytes(path);  //读取文件到字节数组
+        byte[] bytes = File.ReadAllBytes(fileName);  //读取文件到字节数组
         FileSizeBytes = bytes.Length;
-        string text = Encoding.UTF8.GetString(bytes);  //转为文本，方便分离文件头部信息
 
-        GetHeader(text);
+        ReadHeader(fileName, out var dataBytesIndex);
 
-        string dataType = header.DATA.Split(' ')[1].TrimEnd();  //储存的数据类型，ascii binary binary_compressed
+        string dataType = header.data.Split(' ')[1].TrimEnd();  //储存的数据类型，ascii binary binary_compressed
 
-        var sizes = header.SIZE
+        var sizes = header.size
             .Split(' ')
             .Skip(1)
             .Select(item => int.Parse(item));
-        var counts = header.COUNT
+        var counts = header.count
             .Split(' ')
             .Skip(1)
             .Select(item => int.Parse(item));
         var rowSizeBytes = sizes
             .Zip(counts)
             .Sum(item => item.First * item.Second);
-        pointCount = Convert.ToInt32(header.POINTS.Split(' ')[1]);  //点的数量
-        int index = text.IndexOf(header.DATA) + header.DATA.Length + 1;  //数据开始的索引
-        point3Ds = new List<Point3D>();
+        pointCount = Convert.ToInt32(header.points.Split(' ')[1]);  //点的数量
+        points = new Point3D[pointCount];
 
         switch (dataType)
         {
             case "binary":
-                ReadBinaryData(bytes, rowSizeBytes, index, sizes);
+                ReadBinaryData(bytes, rowSizeBytes, (int)dataBytesIndex, sizes);
                 break;
             case "binary_compressed":
-                ReadBinaryCompressedData(bytes, rowSizeBytes, index);
+                ReadBinaryCompressedData(bytes, rowSizeBytes, (int)dataBytesIndex);
                 break;
             case "ascii":
                 throw new NotImplementedException("Sorry, ASCII data type is not currently supported.");
             default:
                 break;
         }
+    }
+
+    private void ReadHeader(string fileName, out long dataBytesIndex)
+    {
+        var headerLines = new string[10];
+
+        using (var textReader = new StreamReader(fileName))
+        {
+            string? line;
+            var headerLinesIndex = 0;
+            dataBytesIndex = 0;
+            // We understand \n (1 B) is always used; however, \r\n (2 B) could appear too
+            const int newLineBytesLength = 1;
+
+            do
+            {
+                line = textReader.ReadLine();
+
+                if (line == null)
+                {
+                    throw new ArgumentException("PCD is not well-formed.");
+                }
+
+                dataBytesIndex += line.Length + newLineBytesLength;
+
+                if (line.StartsWith('#'))
+                {
+                    continue;
+                }
+
+                headerLines[headerLinesIndex] = line;
+                headerLinesIndex++;
+            }
+            while (!line.StartsWith("DATA"));
+        }
+        
+        header = new Header(headerLines);
     }
 
     private void ReadBinaryCompressedData(byte[] bytes, int rowSizeBytes, int index)
@@ -145,6 +113,8 @@ class PcdReader
         //LZF解压算法
         byte[] data = Decompress(compress, decompressedSize);
         int type = 0;
+        var pointIndex = 0;
+
         for (int i = 0; i < data.Length; i += 4)
         {
             //先读取x坐标
@@ -152,22 +122,34 @@ class PcdReader
             {
                 point = new Point3D();
                 point.x = BitConverter.ToSingle(data, i);
-                point3Ds.Add(point);
-                if (point3Ds.Count == pointCount) type++;
+                points[pointIndex] = point;
+
+                if ((pointIndex + 1) == pointCount)
+                {
+                    type++;
+                }
+
+                pointIndex++;
             }
             else if (type == 1)  //y 坐标
             {
-                point3Ds[i / 4 - pointCount].y = BitConverter.ToSingle(data, i);
+                var point3D = points[i / 4 - pointCount];
+                point3D.y = BitConverter.ToSingle(data, i);
+                points[i / 4 - pointCount] = point3D;
                 if (i / 4 == pointCount * 2 - 1) type++;
             }
             else if (type == 2)   //z 坐标
             {
-                point3Ds[i / 4 - pointCount * 2].z = BitConverter.ToSingle(data, i);
+                var point3D = points[i / 4 - pointCount * 2];
+                point3D.z = BitConverter.ToSingle(data, i);
+                points[i / 4 - pointCount * 2] = point3D;
                 if (i / 4 == pointCount * 3 - 1) type++;
             }
             else if (rowSizeBytes == 4)  //颜色信息
             {
-                point3Ds[i / 4 - pointCount * 3].colorRGB = BitConverter.ToUInt32(data, i);
+                var point3D = points[i / 4 - pointCount * 3];
+                point3D.colorRGB = BitConverter.ToUInt32(data, i);
+                points[i / 4 - pointCount * 3] = point3D;
                 if (i / 4 == pointCount * 4 - 1) break;
             }
         }
@@ -175,37 +157,44 @@ class PcdReader
 
     private void ReadBinaryData(byte[] bytes, int rowSizeBytes, int index, IEnumerable<int> sizes)
     {
-        var fields = header.FIELDS
+        var fields = header.fields
             .Split(' ')
             .Skip(1)
             .Select(item => item.TrimEnd())
             .ToList();
         var colorFieldIndex = fields.IndexOf("rgb");
         var xFieldIndex = fields.IndexOf("x");
+        var xFieldOffset = 4 * xFieldIndex;
         var restOfFields = fields.Except(new string[] { "x", "y", "z", "rgb" });
-        Point3D point;
+        var isLabelAvailable = restOfFields.Contains("label");
+        var labelOffset = GetFieldOffset("label", fields, sizes);
+        var pointIndex = 0;
         
         for (int i = index; i < bytes.Length;)
         {
-            point = new Point3D();
-            point.x = BitConverter.ToSingle(bytes, i + (4 * xFieldIndex));
-            point.y = BitConverter.ToSingle(bytes, i + (4 * xFieldIndex) + 4);
-            point.z = BitConverter.ToSingle(bytes, i + (4 * xFieldIndex) + 8);
+            ref var point = ref points[pointIndex];
+            point.x = BitConverter.ToSingle(bytes, i + xFieldOffset);
+            point.y = BitConverter.ToSingle(bytes, i + xFieldOffset + 4);
+            point.z = BitConverter.ToSingle(bytes, i + xFieldOffset + 8);
+            
             if (colorFieldIndex >= 0)
             {
                 point.colorRGB = BitConverter.ToUInt32(bytes, i + (4 * colorFieldIndex));
             }
 
-            if (restOfFields.Contains("label"))
+            if (isLabelAvailable)
             { 
-                var offset = GetFieldOffset("label", fields, sizes);
                 // TODO support types appart from byte
-                point.label = bytes[i + offset];
+                point.label = bytes[i + labelOffset];
             }
-
-            point3Ds.Add(point);
-            if (point3Ds.Count == pointCount) break;
+            
+            if ((pointIndex + 1) == pointCount)
+            {
+                break;
+            }
+            
             i += rowSizeBytes;
+            pointIndex++;
         }
     }
 
@@ -223,7 +212,7 @@ class PcdReader
     /// <param name="input">要解压的数据</param>
     /// <param name="outputLength">解压之后的长度</param>
     /// <returns>返回解压缩之后的内容</returns>
-    public static byte[] Decompress(byte[] input, int outputLength)
+    private static byte[] Decompress(byte[] input, int outputLength)
     {
         uint iidx = 0;
         uint oidx = 0;
@@ -277,12 +266,16 @@ class PcdReader
 /// <summary>
 /// Pcd 中的点
 /// </summary>
-class Point3D
+struct Point3D
 {
     public float x;
+
     public float y;
+
     public float z;
+
     public uint colorRGB;
+
     public byte label;
 }
 
@@ -291,22 +284,58 @@ class Point3D
 /// </summary>
 class Header
 {
-    public string FistLine = "";  //pcd 文件的第一行
-    public string VERSION;
-    public string FIELDS;
+    public string version;
+
+    public string fields;
+
     /// <summary>
-    /// Size of each dimension (<see cref="FIELDS"/>) in bytes.
+    /// Size of each dimension (<see cref="fields"/>) in bytes.
     /// </summary>
-    public string SIZE;
-    public string TYPE;
-    public string COUNT;
-    public string WIDTH;
-    public string HEIGHT;
+    public string size;
+
+    public string type;
+
+    public string count;
+
+    public string width;
+
+    public string height;
+
     /// <summary>
     /// An acquisition viewpoint for the points in the dataset, 
     /// specified as a translation (tx ty tz) + quaternion (qw qx qy qz).
     /// </summary>
-    public string VIEWPOINT;
-    public string POINTS;
-    public string DATA;
+    public string viewpoint;
+
+    public string points;
+
+    public string data;
+
+    public Header(params string[] headerLines)
+    {
+        version = headerLines[0];
+        fields = headerLines[1];
+        size = headerLines[2];
+        type = headerLines[3];
+        count = headerLines[4];
+        width = headerLines[5];
+        height = headerLines[6];
+        viewpoint = headerLines[7];
+        points = headerLines[8];
+        data = headerLines[9];
+        var viewpointParts = viewpoint.Split(' ').Skip(1);
+        ViewpointTranslation = (
+            viewpointParts.ElementAt(0),
+            viewpointParts.ElementAt(1),
+            viewpointParts.ElementAt(2));
+        ViewpointQuaternion = (
+            viewpointParts.ElementAt(3),
+            viewpointParts.ElementAt(4),
+            viewpointParts.ElementAt(5),
+            viewpointParts.ElementAt(6));
+    }
+
+    public (string x, string y, string z) ViewpointTranslation { get; private set; }
+
+    public (string w, string x, string y, string z) ViewpointQuaternion { get; private set; }
 }
